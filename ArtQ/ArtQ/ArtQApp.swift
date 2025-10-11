7e45921146bd7b328500affc671310c1ab89a75e
kodaylego <kodaylego@gmail.com>
2025-10-11 21:27:01 +0900

    create project

---
+++ ArtQ/ArtQ/ArtQApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtQApp.swift
+//  ArtQ
+//
+//  Created by koday on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ArtQApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ ArtQ/ArtQTests/ArtQTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtQTests.swift
+//  ArtQTests
+//
+//  Created by koday on 10/11/25.
+//
+
+import Testing
+@testable import ArtQ
+
+struct ArtQTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
